5f6c92c49339ac445fd0684e28d7c0edb8493a4d
Sunny <sunny4823@kakao.com>
2020-09-21 11:29:30 +0900

    sw.js

--- _site/app.js
+++ _site/app.js
@@ -1,1 +1,7 @@
-/* Registering Service Worker */ if('serviceWorker' in navigator) { navigator.serviceWorker.register('/sw.js?v=1'); };
+/* Registering Service Worker */
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('{{ "/sw.js" | relative_url }}').then(reg => {
+    // sometime later…
+    reg.update();
+  });
+};
