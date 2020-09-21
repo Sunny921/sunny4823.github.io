f39b38bf00bdccc55e03f1b7881a82a608707caa
Sunny <sunny4823@kakao.com>
2020-09-21 11:28:39 +0900

    sw.js

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
----
-layout: compress
-# Chirpy v2.2
-# https://github.com/cotes2020/jekyll-theme-chirpy
-# © 2020 Cotes Chung
-# MIT Licensed
----
-
 /* Registering Service Worker */
 if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('{{ "/sw.js?v=1" | relative_url }}');
+  navigator.serviceWorker.register('{{ "/sw.js" | relative_url }}').then(reg => {
+    // sometime later…
+    reg.update();
+  });
 };
