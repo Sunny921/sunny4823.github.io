9129a61f33d93b4fee39dc60eac99e9c05bd5239
Sunny <sunny4823@kakao.com>
2020-09-21 11:44:33 +0900

    sw.js

--- _site/app.js
+++ _site/app.js
@@ -1,6 +1,6 @@
 /* Registering Service Worker */
 if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('{{ "/sw.js" | relative_url }}').then(reg => {
+  navigator.serviceWorker.register('sw.js').then(reg => {
     // sometime later…
     reg.update();
   });
